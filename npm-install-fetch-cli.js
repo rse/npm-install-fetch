14953f6cc30be2beda3c410de61d9e283ceccf96
Ralf S. Engelschall <rse@engelschall.com>
2022-04-03 15:57:42 +0200

    update year in all copyright messages

--- npm-install-fetch-cli.js
+++ npm-install-fetch-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  NPM-Install-Fetch -- Fetch External Resources on NPM Package Installation
-**  Copyright (c) 2018-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2018-2022 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
@@ -78,7 +78,7 @@ const fetch      = require("./npm-install-fetch-api.js")
     if (argv.version) {
         process.stderr.write(`${my.name} ${my.version} <${my.homepage}>\n`)
         process.stderr.write(`${my.description}\n`)
-        process.stderr.write(`Copyright (c) 2018-2020 ${my.author.name} <${my.author.url}>\n`)
+        process.stderr.write(`Copyright (c) 2018-2022 ${my.author.name} <${my.author.url}>\n`)
         process.stderr.write(`Licensed under ${my.license} <http://spdx.org/licenses/${my.license}.html>\n`)
         process.exit(0)
     }
